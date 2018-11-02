374ac6488329697937a48a75c2870ddf51e341eb
Alicia Alonso Garcia <alalonso@csumb.edu>
2018-11-02 18:18:08 +0000

    addig lab 6 again

--- labs/lab6/loginProcess.php
+++ labs/lab6/loginProcess.php
@@ -1,47 +1,57 @@
 <?php
-session_start();
+session_start();  //starts or resumes a session
 
+//verifies that username and password are valid
 
-//verifies that username and passord is valid
+ include '../../sqlconnection.php';
+ $dbConn = getConnection("quotes");
 
-include '../../sqlconnection.php';
-$dbConn = getConnection("quotes");
+ $username = $_POST['username'];
+ $password = sha1($_POST['password']);
 
+//This sql works but allows SQL INJECTION!! (BECAUSE OF THE SINGLE QUOTES)
+ $sql = "SELECT * 
+         FROM q_admin 
+         WHERE username = '$username' 
+         AND   password = '$password' ";
 
-$username = $_POST['username'];
-$password = sha1($_POST['password']);
+//This sql prevents SQL INJECTION!!
+ $sql = "SELECT * 
+         FROM q_admin 
+         WHERE username = :u;
+         AND   password = :password ";
 
-$sql = "SELECT *
-        FROM q_admin
-        WHERE username = :username
-        AND password = :password ";
-        
-$namedParameters = array();
-$namedParameters[":username"] = $username;
-$namedParameters[":password"] = $password;
+ $namedParameters = array();
+ $namedParameters[":u"] = $username;
+ $namedParameters[":password"] = $password;
+         
+ //echo $sql;
+ $stmt = $dbConn->prepare($sql);
+ $stmt->execute($namedParameters);
+ $record = $stmt->fetch(PDO::FETCH_ASSOC); //we're expecting just one record
+ 
+ //print_r($record);
+ if (empty($record)){
+     
+     echo "Error: Wrong Username or Password!!";
+     
+ } else {
 
-//echo $sql;
-$stmt = $dbConn->prepare($sql);
-$stmt->execute($namedParameters);
-$record = $stmt->fetch(PDO::FETCH_ASSOC);
-
-//print_r($record);
-
-if(empty($record)){ 
-    
-    echo "Error: Wrong Username or Password!";
-    
-    
-} else {
-    
-        $_SESSION['adminName'] = $record['firstName'] . " " . $record['lastName'];
-    
-    header("location: main.php");
-    
-    
-}
+     $_SESSION['adminName'] = $record['firstName'] . " " . $record['lastName'];
+     
+     header("location: main.php"); //redirects to another program.
+     
+ }
+ 
+ 
+ 
+ 
 
 ?>
+  
+  
+
+
 
 <style>
             h1,body{
